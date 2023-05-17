1c445a0b9ef4ab2e4639534b70d9cda58d15e494
Hemant kumar <116268777+hemant0933@users.noreply.github.com>
2023-05-17 14:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+ document.getElementById('myForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
+      
+      var name = document.getElementById('name').value;
+      var year = document.getElementById('year').value;
+      var url = document.getElementById('url');
+      
+      // Update the URL with the appropriate query string
+      url.textContent = 'https://localhost:8080/?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
+    });
